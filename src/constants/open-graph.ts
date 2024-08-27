06e812fd97d8d16ebae39ff7c06e0a664576135f
shion <shionlk98@gmail.com>
2024-08-27 19:02:45 -0300

    update

--- src/constants/open-graph.ts
+++ src/constants/open-graph.ts
@@ -7,15 +7,15 @@ import type { Metadata } from "next";
 export const siteMetadata: Metadata = {
 
   title: "Pedro | Buzzvel Test",
-  description: "This is a test page for Buzzvel, created by Pedro Maciel.",
+  description: "This is a test page for Buzzvel-test, created by Pedro Maciel.",
   openGraph: {
     title: "Pedro | Buzzvel Test",
-    description: "Discover Pedro's test page for Buzzvel, showcasing expertise in modern web development.",
+    description: "Discover Pedro's test page for Buzzvel.",
     url: "https://buzzvel-test-five.vercel.app/pt", 
     type: "website",
     images: [
       {
-        url: "/img/op-image.png", 
+        url: "/img/opengraph-image.png", 
         width: 1200,
         height: 630,
         alt: "Pedro | Buzzvel Test Image",
@@ -26,8 +26,8 @@ export const siteMetadata: Metadata = {
   twitter: {
     card: "summary_large_image",
     title: "Pedro | Buzzvel Test",
-    description: "Discover Pedro's test page for Buzzvel, showcasing expertise in modern web development.",
-    images: "/img/op-image.png", 
+    description: "Discover Pedro's test page for Buzzvel.",
+    images: "/img/opengraph-image.png", 
   },
 
   
